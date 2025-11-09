03daed34e1de3ebd4f7779f6d036c0cb9c5f99ee
Krispy125 <kburns19@rockets.utoledo.edu>
2025-11-08 19:04:18 -0500

    Added a few comments and screen size configuration

--- VoxelProject/main.cpp
+++ VoxelProject/main.cpp
@@ -1,13 +1,18 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_LENGTH = 600;
+
 int main() {
+
+	// glfw Config
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "VoxelProject", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_LENGTH, "VoxelProject", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Initialization Failed" << std::endl;
@@ -36,8 +41,24 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow* window) // Input Processing and Close Button
+void processInput(GLFWwindow* window) // Input Processing
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+	while (!glfwWindowShouldClose(window))
+	{
+		// Input
+		processInput(window); 
+
+		// Rendering Commands
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+
+		// Check and Call Events
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 }
